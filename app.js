8f6d6a0732f7a6bc1d9315f746e53118e61ddf3e
Jake Lehoullier <jakelehou@vt.edu>
2024-04-02 16:09:06 -0400

    Initialize project using Create React App

---
+++ app.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import TypingEffect from '../home'; 
+
+function App() {
+  return (
+    <div className="App">
+      <TypingEffect />
+    </div>
+  );
+}
+
+export default App;
